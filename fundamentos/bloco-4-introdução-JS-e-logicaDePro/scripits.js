3470af2b2e6d17f6dc2015fdb823506ab1159a9c
victor marques <victormarques.ym@gmail.com>
2022-06-21 16:20:23 -0300

    adiciona primeiro exercercicio bloco 4 JS

---
+++ fundamentos/bloco-4-introdução-JS-e-logicaDePro/scripits.js
@@ -0,0 +1,9 @@
+const  myName = "victor";
+const  birthCity = "belém";
+let  birthYear = 1996;
+birthYear = 230;
+birthCity = "marituba";
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);

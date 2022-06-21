8851943521f25b6595b5afab17344e78d91e6699
victor marques <victormarques.ym@gmail.com>
2022-06-21 20:51:31 -0300

    mais um exercicio do bloco-4

---
+++ fundamentos/bloco-4-introdução-JS-e-logicaDePro/bloco-4-exer-4.js
@@ -0,0 +1,8 @@
+let weeDay = "quarta-feira";
+
+if(weeDay = "segunda-feira" || "terça-feira" || "quarta-feira" || "quinta-feira" || "sexta-feira") {
+console.log( "Oba, mais um dia de aprendizado na Trybe >:D")
+}
+if(weeDay = "sabado"){
+console.log("FINALMENTE, descanso merecido UwU")
+}

7e55251c3f50c356fe7a3d739c5d3356d1f71192
victor marques <victormarques.ym@gmail.com>
2022-06-21 20:15:24 -0300

    adiciona exercicio do bloco 4.

---
+++ fundamentos/bloco-4-introdução-JS-e-logicaDePro/bloco-4-dia-1-exer.js
@@ -0,0 +1,7 @@
+const nota = 50;
+if (nota >= 80);
+console.log("Parabéns, você foi aprovada(o)!");
+ if(nota < 80 &&  60);
+ console.log("Você está na nossa lista de espera");
+ if(nota < 60);
+ console.log("Você foi reprovada(o)");

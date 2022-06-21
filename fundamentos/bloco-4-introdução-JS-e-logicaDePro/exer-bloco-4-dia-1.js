224e52145e32a1c1ed5f60cc966c0fd5d3293408
victor marques <victormarques.ym@gmail.com>
2022-06-21 20:35:31 -0300

    exercicio-bl-4-dia-1-

---
+++ fundamentos/bloco-4-introdução-JS-e-logicaDePro/exer-bloco-4-dia-1.js
@@ -0,0 +1,11 @@
+const currentHour = 10;
+let message = "";
+
+if(currentHour >= 22);
+message = "Não deveríamos comer nada, é hora de dormir";
+if(currentHour >= 18 && currentHour < 22);
+message = "Rango da noite, vamos jantar :D";
+if(currentHour >= 11 <= 14);
+message = "Hmmm, cheiro de café recém passado";
+
+console.log(message);

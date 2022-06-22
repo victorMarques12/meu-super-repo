b032a82a935d3ece91df50de0433f21497c971d3
victor marques <victormarques.ym@gmail.com>
2022-06-22 13:15:17 -0300

    exercicio 3

---
+++ fundamentos/bloco-4-introdução-JS-e-logicaDePro/bl-4-dia-2-exer-3.js
@@ -0,0 +1,11 @@
+const a = 10;
+const b = 15;
+const c = 20;
+
+if(a > b && a > c){
+    console.log("o maior número é: ' + a + ' (a)");
+}else if(b > a && b > c){
+    console.log("o maior núnemro é: ' + b + '(b)");
+}else{
+    console.log("o maior número é: ' + c + ' (c)");
+}

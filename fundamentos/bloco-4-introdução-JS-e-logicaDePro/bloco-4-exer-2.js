4beda470a3f130c02ddb7658dd680d5adc56133b
victor marques <victormarques.ym@gmail.com>
2022-06-22 12:34:50 -0300

    exercicio do bloco 4 dia 1

---
+++ fundamentos/bloco-4-introdução-JS-e-logicaDePro/bloco-4-exer-2.js
@@ -0,0 +1,11 @@
+const myName = "victor";
+const birthCity = "para"
+let = birthYear = 1996;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+birthYear = 2030;
+
+console.log(birthYear);

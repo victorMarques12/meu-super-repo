9e9872a356886adbb90a96774bd17aaea9b45991
victor marques <victormarques.ym@gmail.com>
2022-06-22 10:40:04 -0300

    bloco- 4- exercio-6

--- fundamentos/bloco-4-introdução-JS-e-logicaDePro/bloco-4-exer-5.js
+++ fundamentos/bloco-4-introdução-JS-e-logicaDePro/bloco-4-exer-5.js
@@ -1,6 +1,16 @@
-let candidato = "";
+let candidato = "reprovado";
 
 switch(candidato){
-    case "aprovado"
-    console.log("meus parabens");
+    case "aprovado":
+    console.log("com distinção");
+    break;
+   
+    case "reprovado":
+    console.log("boa sorte na proxima");
+    break;
+       
+    case "list":
+    console.log("voce ainda tem uma chance");
+    break;
+
 }

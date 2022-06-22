16cd9ff31c25d1c85e7410dd1022da10424efbaf
victor marques <victormarques.ym@gmail.com>
2022-06-22 12:35:27 -0300

    alteraçoes no codigo

--- fundamentos/bloco-4-introdução-JS-e-logicaDePro/bloco-4-dia-1-exer.js
+++ fundamentos/bloco-4-introdução-JS-e-logicaDePro/bloco-4-dia-1-exer.js
@@ -1,7 +1,8 @@
-const nota = 50;
+const nota = 60;
+
 if (nota >= 80);
 console.log("Parabéns, você foi aprovada(o)!");
- if(nota < 80 &&  60);
+ if(nota < 80 || nota >= 60);
  console.log("Você está na nossa lista de espera");
  if(nota < 60);
  console.log("Você foi reprovada(o)");

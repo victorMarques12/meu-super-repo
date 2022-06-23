3f6a944eb250d7525bd229d00755e7975d4ec6bd
victor marques <victormarques.ym@gmail.com>
2022-06-23 16:20:17 -0300

    academia de logica

---
+++ fundamentos/bloco-4-introdução-JS-e-logicaDePro/bl-4-js-logica-progrmacao.js
@@ -0,0 +1,11 @@
+let fruits = [3, 4, 10, 1, 12];
+let valor = 0;
+
+for(let i = 0; i < fruits.length ; i += 1){
+        console.log(valor += fruits[i]);
+}
+    if(valor > 15){
+        console.log(valor);
+    }else{    
+        console.log("valor menor que 16");
+    }

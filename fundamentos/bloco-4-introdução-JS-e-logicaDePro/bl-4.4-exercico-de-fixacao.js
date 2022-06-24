f9327dc6862dde4f93084edd1725c7a6cb26748b
victor marques <victormarques.ym@gmail.com>
2022-06-24 18:28:54 -0300

    aula do dia 24

---
+++ fundamentos/bloco-4-introdução-JS-e-logicaDePro/bl-4.4-exercico-de-fixacao.js
@@ -0,0 +1,8 @@
+let car = {
+    type: 'Fiat',
+    model: '500',
+    color: 'white',
+  };
+
+
+  console.log('o carro da '+ car.type + ' ' + 'modelo' + ' ' + car.model + ' ' + 'da cor'+ ' ' + car.color);
